51902c02d32583762667ee680cf2bd2f0bb3d519
go2jame <go2jame@gmail.com>
2016-10-23 16:07:28 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function(){  // On page load
+
+$(window).scroll(function () {
+     var sc = $(window).scrollTop()
+    if (sc > 100) {
+        $("#header-sroll").addClass("header-small")
+    } else {
+        $("#header-sroll").removeClass("header-small")
+    }
+});
+
+  $("#Carousel").owlCarousel({
+    //navigation : true,  Show next and prev buttons
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    singleItem:true,
+    responsive:true
+  });
+
+$('.main-menu a,#mobile-menu a').click(function(event) {
+    event.preventDefault();
+    var link = this;
+    $.smoothScroll({
+      scrollTarget: link.hash
+    });
+    window.location.hash
+    window.history.pushState("string", "Title",window.location.pathname+ window.location.search+link.hash);
+    //console.log(window.location.pathname+ window.location.search+link.hash);
+});
+
+$('#mobile-menu').slicknav({
+    prependTo:'.mobile-menu-wrapper',
+    brand: '<img src="img/logo.png" height="34">',
+    closeOnClick: true
+});
+
+});
+
